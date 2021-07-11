d28d2f8694134f82e23f081c3aa7c755d3d39f66
andresdiegolanda <andresdiegolanda@gmail.com>
2021-07-11 01:03:04 +0100

    Added Cucumber report generation

--- src/test/java/com/adlanda/CucumberIntegrationTest.java
+++ src/test/java/com/adlanda/CucumberIntegrationTest.java
@@ -6,6 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/")
+@CucumberOptions(features = "src/test/resources/",
+plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json" })
 public class CucumberIntegrationTest {
 }
